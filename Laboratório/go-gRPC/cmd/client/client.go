61a98d32ab7c70c8617994ee4c5618e389f36fe6
github-carlos <carlos@MacBook-Air-de-Carlos.local>
2022-09-13 10:44:17 -0300

    created client to request grpc server

---
+++ Laboratório/go-gRPC/cmd/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/github-carlos/Desafios-Full-Cycle/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect to Server: %v", err)
+	}
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Carlos Eduardo",
+		Email: "carlos@email.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make request to Server: %v", err)
+	}
+
+	fmt.Print(res)
+}
--- Laboratório/go-gRPC/cmd/server/server.go
+++ Laboratório/go-gRPC/cmd/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/github-carlos/Desafios-Full-Cycle/pb"
 	"github.com/github-carlos/Desafios-Full-Cycle/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -18,6 +19,8 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+
+	reflection.Register(grpcServer)
 
 	pb.RegisterUserServiceServer(grpcServer, &services.UserService{})
 
