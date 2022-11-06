eac95758155ca6423e65c8ead2098329f0858529
github-carlos <carlos@MacBook-Air-de-Carlos.local>
2022-11-06 18:51:23 -0300

    created product model for database

---
+++ Laboratório/DDD na pratica/src/infra/repository/product.repository.spec.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize-typescript";
+
+describe("#ProductRepository", () => {
+
+  let sequelize: Sequelize;
+
+  beforeEach(async() => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true },
+    })
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+});
