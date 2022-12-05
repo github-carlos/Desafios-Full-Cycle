9e16836bb2a4bd1ec5b2a181311b7f975b0510ab
github-carlos <carlos@MacBook-Air-de-Carlos.local>
2022-12-05 08:30:10 -0300

    wip: creating domain events

---
+++ Laboratório/DDD na pratica/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,17 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handlers/send-email-when-product-is-created.handler";
+import EventDispatcher from "./event-dispatcher";
+
+describe('Domain Events tests', () => {
+
+
+  it("should register an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+  });
+
+});
