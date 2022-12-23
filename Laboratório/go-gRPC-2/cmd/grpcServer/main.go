8fd2dacd94aee598b3834cb97506ddc9500f55e2
github-carlos <carlos@MacBook-Air-de-Carlos.local>
2022-12-23 08:41:36 -0300

    created server grpc and opened tcp connection

---
+++ Laboratório/go-gRPC-2/cmd/grpcServer/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"example.com/m/internal/database"
+	"example.com/m/internal/pb"
+	"example.com/m/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	// fazendo conexao com o banco
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	// dizendo para fechar a conexao quando tudo finalizar
+	defer db.Close()
+
+	// instanciando o repositorio
+	categoryDb := database.NewCategory(db)
+	// instanciando o servico
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	// criando o grpc server
+	grpcServer := grpc.NewServer()
+	// registrando nosso servico no grpc server
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	// abrindo uma conexao tcp
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
+}
